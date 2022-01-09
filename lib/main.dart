aa06bb8555183cd2788d55810e6423c7cb2830ab
donmelaka <kalawila.gunasekara@reece.com.au>
2022-01-09 18:36:31 +1100

    Refactored and added animations

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,17 @@ class FlashChat extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
+          bodyText1: TextStyle(color: Colors.black54),
         ),
       ),
       home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        ChatScreen.id: (ctx) => ChatScreen(),
+        LoginScreen.id: (ctx) => LoginScreen(),
+        RegistrationScreen.id: (ctx) => RegistrationScreen(),
+        WelcomeScreen.id: (ctx) => WelcomeScreen(),
+      },
     );
   }
 }
